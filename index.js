fd293b33b389471651e7a31df96d626860da1ae4
Md. Istehad Ahmed Rafi <istehadrafi09@gmail.com>
2024-09-01 11:20:43 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const port = 8000;
+
+const app = express();
+
+app.set("view engine","ejs");
+
+app.listen(port,() =>{
+    console.log(`Server is running on port ${port}`);
+})
