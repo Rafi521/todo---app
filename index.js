ffcc1dd4c2c2f7f5e771982f31e7add5d263ad03
Md. Istehad Ahmed Rafi <istehadrafi09@gmail.com>
2024-09-02 12:24:00 +0600

    Added TODO application files and updated configuration

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-const express = require("express");
 
+const app = require("./app");
 const port = 8000;
-
-const app = express();
-
-app.set("view engine","ejs");
-
 app.listen(port,() =>{
     console.log(`Server is running on port ${port}`);
 })
